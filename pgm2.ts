40a96e0d8c456f4b2b85c4b463e33ad639f150bf
karthikfsd566-ctrl <karthikfsd566@gmail.com>
2026-08-15 18:40:41 +0530

    Add files via upload

---
+++ pgm2.ts
@@ -0,0 +1,13 @@
+var num = 2 ;
+if (num == 1) {
+    console.log ("typscript statement 1");
+}
+else if (num == 2) {
+    console.log ("typescript statement 2");
+}
+else if (num == 3) {
+    console.log ("typescript statement 3");
+}
+else {
+    console.log ("typescript statement n");
+}
